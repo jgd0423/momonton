d8c36934b28ea97d50045a9e7baf4d18fa378775
Jeong_geumdam <suseongland@gmail.com>
2020-08-07 22:33:07 +0900

    #3.1 Making a JS Clock part Two

--- clock.js
+++ clock.js
@@ -3,14 +3,18 @@ const clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
   const date = new Date();
-  const minutes = date.getMinutes();
   const hours = date.getHours();
+  const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  const zeroPlusHours = hours < 10 ? `0${hours}` : hours;
+  const zeroPlusMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const zeroPlusSeconds = seconds < 10 ? `0${seconds}` : seconds;
+  clockTitle.innerText = `${zeroPlusHours}:${zeroPlusMinutes}:${zeroPlusSeconds}`;
 }
 
 function init() {
   getTime();
+  setInterval(getTime, 1000);
 }
 
 init();
