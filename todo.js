c693e57718f7ce0efd4047560a316c025de3ef59
Jeong_geumdam <suseongland@gmail.com>
2020-08-09 15:43:29 +0900

    #3.5 Making a To Do List part Two

--- todo.js
+++ todo.js
@@ -1,20 +1,37 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-  toDoInput =  toDoForm.querySelector("input"),
+  toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+// toDos를 string으로 변환해 로컬 스토리지에 저장한다
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+// ul class "js-toDoList"안에 li를 생성해 넣고, toDoObj을 생성해 toDos에 넣는다
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
   delBtn.innerText = "✖";
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
+// 로컬 스토리지에 새로 값이 추가될 때마다 paintToDo한다
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
@@ -22,10 +39,14 @@ function handleSubmit(event) {
   toDoInput.value = "";
 }
 
+// 로컬 스토리지에 저장된 값이 있으면 paintToDo한다
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
-
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 
