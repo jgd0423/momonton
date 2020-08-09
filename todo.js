9b7b3ff3c6ad884806a6950a0a9d007f38b0da18
Jeong_geumdam <suseongland@gmail.com>
2020-08-09 18:25:44 +0900

    #3.6 Making a To Do List part Three

--- todo.js
+++ todo.js
@@ -4,7 +4,19 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+let toDos = [];
+
+// 버튼을 누르면 html에서 사라지게 하고 로컬 스토리지에서도 사라지게 한다
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 // toDos를 string으로 변환해 로컬 스토리지에 저장한다
 function saveToDos() {
@@ -12,12 +24,14 @@ function saveToDos() {
 }
 
 // ul class "js-toDoList"안에 li를 생성해 넣고, toDoObj을 생성해 toDos에 넣는다
+// delBtn을 누르면 deletoToDo를 실행한다
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerText = "✖";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
