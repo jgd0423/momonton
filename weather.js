81c487fa8189a55f360072433251cb144301d371
Jeong_geumdam <suseongland@gmail.com>
2020-08-10 14:24:09 +0900

    #3.8 Getting the Weather part One Geolocation

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "edde74c32c238c67ca29b141f037163b";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+  }
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Can't access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    // getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
